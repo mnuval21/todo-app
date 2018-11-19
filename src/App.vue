208f0e8097777477b36fe77e7deab089f47b6c96
melissa <melissa@ecquire.com>
2018-11-19 10:11:51 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+
+export default {
+  components: {
+    TodoList,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <div>
+
+    <ul>
+      <li> Todo A </li>
+      <li> Todo B </li>
+      <li> Todo C </li>
+    </ul>
+
+  </div>
+
+</template>
+
+<script type = "text/javascript">
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+  },
+
+
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      },{
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      },{
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      },{
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
+
+
+</script>
+
+
+<style>
+
+</style>
